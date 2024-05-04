9889ec8e60a1f8172ceeae24e721ae21f361d60f
sallescosta <newcapital.in@gmail.com>
2024-05-04 14:08:18 +0200

    feat: add jwt generation and product validation by token

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/jwtauth"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/sallescosta/crud-api/internal/entity"
+	"github.com/sallescosta/crud-api/internal/infra/database"
+	"github.com/sallescosta/crud-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 
 	"github.com/sallescosta/crud-api/configs"
 )
 
 func main() {
 	config, _ := configs.LoadConfig(".")
-	fmt.Println(config.DBDriver)
 
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productHandler := handlers.NewProductHandler(database.NewProduct(db))
+	userHandler := handlers.NewUserHandler(database.NewUser(db), config.TokenAuth, config.JWTExpiresIn)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Get("/", productHandler.GetProducts)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.CreateUser)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
